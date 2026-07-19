6e875c1416217c0fd2c2c9491e3c2baed8b755f2
pathareshubham98-dot <pathareshubham98@gmail.com>
2026-07-19 20:06:58 +0530

    Build Healthy Food App using React Fragments and Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,17 @@ import FoodItems from "./components/FoodItems";
 import ErrorMessage from "./components/Errormessage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Container from "./components/Container";
 
 function App() {
-  let foodItems = [
-    "Dal",
-    "Green Vegetable",
-    "Roti",
-    "Salad",
-    "Milk",
-    "Ghee",
-  ];
+  let foodItems = ["Sabzi", "Green Vegetable", "Roti", "Salad", "Milk", "Ghee"];
 
   return (
-    <>
+    <Container>
       <h1 className="food-heading">Healthy Food</h1>
       <ErrorMessage items={foodItems} />
       <FoodItems items={foodItems} />
-    </>
+    </Container>
   );
 }
 
--- src/components/item.jsx
+++ src/components/item.jsx
@@ -2,7 +2,7 @@ import styles from "./Item.module.css";
 
 const Item = ({ foodItem }) => {
   return (
-    <li className={`list-group-item ${styles["kg-item"]}`}>
+    <li className={`list-group-item ${styles["kg-item"]} list-group-item`}>
       <span className={styles["kg-span"]}>{foodItem}</span>
     </li>
   );
