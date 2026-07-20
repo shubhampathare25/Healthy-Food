c0064a7cb4cd73cceb4e01c732b647fc565af7f2
pathareshubham98-dot <pathareshubham98@gmail.com>
2026-07-20 12:51:51 +0530

    search bar food item

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ErrorMessage from "./components/Errormessage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Container from "./components/Container";
+import FoodInput from "./components/FoodInput";
 
 function App() {
   let foodItems = ["Sabzi", "Green Vegetable", "Roti", "Salad", "Milk", "Ghee"];
@@ -12,6 +13,7 @@ function App() {
       <Container asdf="asdfasdf">
         <h1 className="food-heading">Healthy Food</h1>
         <ErrorMessage items={foodItems}></ErrorMessage>
+        <FoodInput></FoodInput>
         <FoodItems items={foodItems}></FoodItems>
       </Container>
 
